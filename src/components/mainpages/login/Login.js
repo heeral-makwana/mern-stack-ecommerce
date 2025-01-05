14f36371d1a107963469423a74ac767c95d1e706
heeral-makwana <makwanahiral60@gmail.com>
2025-01-05 18:48:37 +0530

    updated css

--- src/components/mainpages/login/Login.js
+++ src/components/mainpages/login/Login.js
@@ -1,45 +1,39 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
+import './form.css'  // Adjust path based on your folder structure
 
 const Login = () => {
-  const [user,setUser] = useState({
-    email:'',
-    password:''
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
   })
 
-  const onChangeInput = e => {
-    const {name,value} = e.target;
-    setUser({...user,[name]:value})
+  const onChangeInput = (e) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
   }
 
-  const loginSubmit =async e => {
+  const loginSubmit = async (e) => {
     e.preventDefault()
-    try{
-      await axios.post('https://e-commerce-back-8kja.onrender.com/user/login',{...user})
-
-      localStorage.setItem('firstLogin',true)
-
+    try {
+      await axios.post('https://e-commerce-back-8kja.onrender.com/user/login', { ...user })
+      localStorage.setItem('firstLogin', true)
       window.location.href = "/"
-      
-    }catch(err){
+    } catch (err) {
       alert(err.response.data.msg)
     }
   }
 
-
   return (
-    <div className='login-page'>
+    <div className='form-container'>
       <form onSubmit={loginSubmit}>
-        <input type='email' name='email' required placeholder='Email' value={user.email} onChange={onChangeInput}/>
-        <input type='password' name='password' required placeholder='Password' value={user.password} onChange={onChangeInput}/>
-
+        <input type='email' name='email' required placeholder='Email' value={user.email} onChange={onChangeInput} />
+        <input type='password' name='password' required placeholder='Password' value={user.password} onChange={onChangeInput} />
+        <button type='submit'>Login</button>
         <div className='row'>
-          <button type='submit'>Login</button>
           <Link to='/register'>Register</Link>
         </div>
-
-
       </form>
     </div>
   )
--- src/components/mainpages/login/Register.js
+++ src/components/mainpages/login/Register.js
@@ -1,47 +1,41 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
+import './form.css'  // Adjust path based on your folder structure
 
 const Register = () => {
-  const [user,setUser] = useState({
-    name:'',
-    email:'',
-    password:''
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: ''
   })
 
-  const onChangeInput = e => {
-    const {name,value} = e.target;
-    setUser({...user,[name]:value})
+  const onChangeInput = (e) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
   }
 
-  const registerSubmit =async e => {
+  const registerSubmit = async (e) => {
     e.preventDefault()
-    try{
-      await axios.post('https://e-commerce-back-8kja.onrender.com/user/register',{...user})
-
-      localStorage.setItem('firstLogin',true)
-
+    try {
+      await axios.post('https://e-commerce-back-8kja.onrender.com/user/register', { ...user, withCredentials: true })
+      localStorage.setItem('firstLogin', true)
       window.location.href = "/"
-      
-    }catch(err){
+    } catch (err) {
       alert(err.response.data.msg)
     }
   }
 
-
   return (
-    <div className='register-page'>
+    <div className='form-container'>
       <form onSubmit={registerSubmit}>
-        <input type='text' name='name' required placeholder='Name' value={user.name} onChange={onChangeInput}/>
-        <input type='email' name='email' required placeholder='Email' value={user.email} onChange={onChangeInput}/>
-        <input type='password' name='password' required placeholder='Password' value={user.password} onChange={onChangeInput}/>
-
+        <input type='text' name='name' required placeholder='Name' value={user.name} onChange={onChangeInput} />
+        <input type='email' name='email' required placeholder='Email' value={user.email} onChange={onChangeInput} />
+        <input type='password' name='password' required placeholder='Password' value={user.password} onChange={onChangeInput} />
+        <button type='submit'>Register</button>
         <div className='row'>
-          <button type='submit'>Register</button>
           <Link to='/login'>Login</Link>
         </div>
-
-
       </form>
     </div>
   )
